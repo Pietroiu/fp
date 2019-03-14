0948a779bd8255f17b23127963eb73ff7b0aa5fb
Pietroiu <pietroiu.alexandru@gmail.com>
2019-03-14 20:06:16 +0200

    Update equals.test.js test messages

--- tests/equals.test.js
+++ tests/equals.test.js
@@ -1,19 +1,19 @@
 import {equals} from './../modules/equals';
 
-test("deep object assign",()=>{
+test("deep object equality",()=>{
 	let a = {a:{b:1},c:2,d:[3,4,5]};
 	let b = {a:{b:1},c:2,d:[3,4,5]};
 	expect(equals(a,b)).toBeTruthy();
 });
 
-test("deep object assign",()=>{
+test("number equality",()=>{
 	expect(equals(1,1)).toBeTruthy();
 });
 
-test("deep object assign",()=>{
+test("number inequality",()=>{
 	expect(equals(1,2)).toBeFalsy();
 });
 
-test("deep object assign",()=>{
+test("array equality",()=>{
 	expect(equals([1,2,3],[1,2,3])).toBeTruthy();
 });

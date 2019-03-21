99504f50db809b38c518d96aa9eedf4ad6a2cdee
Pietroiu <pietroiu.alexandru@gmail.com>
2019-03-21 23:45:04 +0200

    Minify copy function

--- modules/copy.js
+++ modules/copy.js
@@ -4,10 +4,6 @@ import {isPrimitive} from "./isPrimitive";
 
 export let copy = function(o){
 	if (isPrimitive(o)) return o;
-	if (isObject(o)) return Object.keys(o).reduce(function(acc,k){
-		return {...acc,[k]:copy(o[k])};
-	},{});
-	if (isArray(o)) return o.map(function(v){
-		return copy(v);
-	});
+	if (isObject(o)) return Object.keys(o).reduce((acc,k)=>({...acc,[k]:copy(o[k])}),{});
+	if (isArray(o)) return o.map((v)=>copy(v));
 };

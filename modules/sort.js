b21d029270826ae3303665a13e8f16aa8948f8a8
Pietroiu <pietroiu.alexandru@gmail.com>
2019-03-22 02:21:45 +0200

    Minify sort function

--- modules/sort.js
+++ modules/sort.js
@@ -1,7 +1,5 @@
 import {curry} from './curry';
 
 export let sort = curry(function(f,a){
-	return a.sort(function (x, y) {
-		return f(x) > f(y) ? 1 : -1;
-	});
+	return a.sort((x, y)=>(f(x) > f(y) ? 1 : -1));
 });

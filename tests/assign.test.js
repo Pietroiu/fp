453f98b5cc5f9bc438e6a0c2277962e1c92c560a
Pietroiu <pietroiu.alexandru@gmail.com>
2019-03-27 01:59:32 +0200

    Update assign test

--- tests/assign.test.js
+++ tests/assign.test.js
@@ -1,6 +1,6 @@
 import {assign} from './../modules/assign';
 
-test("deep object assign",()=>{
+test("Deep object assign",()=>{
 	let a = {a:{b:1},c:2,d:[3,4,5]};
 	let b = {a:{b:5},d:[3,2,2],f:7};
 	let c = {a:{b:5},c:2,d:[3,2,2],f:7};

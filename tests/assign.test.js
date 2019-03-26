63924893172f1cf936df3d71065bbf1344c9bbde
Pietroiu <pietroiu.alexandru@gmail.com>
2019-03-27 01:58:54 +0200

    Update assign test

--- tests/assign.test.js
+++ tests/assign.test.js
@@ -6,9 +6,3 @@ test("deep object assign",()=>{
 	let c = {a:{b:5},c:2,d:[3,2,2],f:7};
 	expect(assign(a,b)).toEqual(c);
 });
-
-test("should return b",()=>{
-	let a = [3,4,5];
-	let b = [0,0];
-	expect(assign(a,b)).toEqual(b);
-});

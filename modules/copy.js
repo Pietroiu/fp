7b477b07f812a847a2f58b6411a1fb95279e9cea
Pietroiu <pietroiu.alexandru@gmail.com>
2019-05-20 17:31:02 +0300

    Update isObject function

--- modules/copy.js
+++ modules/copy.js
@@ -1,9 +1,8 @@
 import {isArray} from './isArray';
 import {isObject} from "./isObject";
-import {isPrimitive} from "./isPrimitive";
 
 export let copy = function(o){
-	if (isPrimitive(o)) return o;
 	if (isObject(o)) return Object.keys(o).reduce((acc,k)=>({...acc,[k]:copy(o[k])}),{});
 	if (isArray(o)) return o.map((v)=>copy(v));
+	return o;
 };

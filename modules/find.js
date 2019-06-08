58bfc3840763fa94112b9d7209f23fabddd87ace
Pietroiu <pietroiu.alexandru@gmail.com>
2019-06-08 20:45:34 +0300

    Update find function

--- modules/find.js
+++ modules/find.js
@@ -1,11 +1,5 @@
 import {curry} from './curry';
 
 export let find = curry(function (fn, a) {
-	if (a) {
-		for (let i = 0; i < a.length; i++) {
-			if (fn(a[i])) return a[i];
-		}
-	} else {
-		return;
-	}
+	return a.find(fn);
 });

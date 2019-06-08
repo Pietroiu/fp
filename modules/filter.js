ff02d309f0bf6caf80792bb46159c4deae491756
Pietroiu <pietroiu.alexandru@gmail.com>
2019-06-08 20:49:05 +0300

    Update filter function

--- modules/filter.js
+++ modules/filter.js
@@ -1,12 +1,5 @@
 import {curry} from './curry';
 
 export let filter = curry(function (fn, a) {
-	let res = [];
-	if (!a) return [];
-	for (let i = 0; i < a.length; i++) {
-		if (a[i] && fn(a[i])) {
-			res = [...res, a[i]];
-		}
-	}
-	return res;
+	return a.filter(fn);
 });

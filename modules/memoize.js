312e817c50a13a1caecb462e1e41f051f58f9398
Pietroiu <pietroiu.alexandru@gmail.com>
2020-06-11 19:04:20 +0300

    Update package.json and package-lock.json and update memoize.js function.

--- modules/memoize.js
+++ modules/memoize.js
@@ -1,8 +1,8 @@
 export let memoize = function (fn) {
 	let cache = {};
 	return (...args) => {
-		let stringifiedArgs = JSON.stringify(args);
-		let result = cache[stringifiedArgs] = cache[stringifiedArgs] || fn(...args);
+		let hash = JSON.stringify(args);
+		let result = cache[hash] = cache[hash] || fn(...args);
 		return result;
 	};
 };

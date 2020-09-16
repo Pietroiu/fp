6b86ed4d9d9853edc8fffb6f7cf8822576c03769
Pietroiu <pietroiu.alexandru@gmail.com>
2020-09-17 02:41:08 +0300

    Update memoize.js

--- modules/memoize.js
+++ modules/memoize.js
@@ -2,7 +2,7 @@ export let memoize = function (fn) {
 	let cache = {};
 	return (...args) => {
 		let hash = JSON.stringify(args);
-		let result = cache[hash] = cache[hash] || fn(...args);
+		let result = cache[hash] || fn(...args);
 		return result;
 	};
 };

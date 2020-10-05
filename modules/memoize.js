4d6724f1a1922746359cb57b1c0866e1f93966fc
Alexandru <pietroiu.alexandru@gmail.com>
2020-10-05 23:53:11 +0300

    Update memoize.js function

--- modules/memoize.js
+++ modules/memoize.js
@@ -1,8 +1,12 @@
-export let memoize = function (fn) {
-	let cache = {};
-	return (...args) => {
-		let hash = JSON.stringify(args);
-		let result = cache[hash] || fn(...args);
-		return result;
-	};
-};
+export let memoize = function(fn) {
+	let memo = {};
+	let slice = Array.prototype.slice;
+	return function() {
+		let args = slice.call(arguments);
+		if (args in memo)
+			return memo[args];
+		else
+			return (memo[args] = fn.apply(this, args));
+
+	}
+}
